1fe8501f5b6640d56848f6aeeac947177c234bd7
andradeviniicius <viniciusdandrade01@gmail.com>
2021-12-01 18:22:21 -0300

    Aula16 - Revisando, muitas duvidas kk

---
+++ 2°Bimestre/ProgramacaoImperativa/Aula16 - Arrow Functions e Callback/aula16 exercicio.js
@@ -0,0 +1,70 @@
+// Exemplo do conteudo
+function nc(nome,sobrenome) {
+  return nome+' '+sobrenome
+}
+function iniciais(nome,sobrenome) {
+    return nome[0] + sobrenome[0];
+}
+function bomDia(nome, sobrenome, callback) {
+    return 'Olá, '+callback(nome,sobrenome)
+}
+console.log(bomDia('Amanda','Ferreira', nc))
+
+
+
+
+
+
+// Exemplo do MDN
+function greeting2(name) {
+    console.log('Hi ' + name);
+}
+
+function greeting(name) {
+    console.log('Hello ' + name);
+  }
+  
+function processUserInput(callback) {
+    var name = console.log('Please enter your name.');
+    callback(name);
+  }
+  
+  processUserInput(greeting2);
+
+
+
+
+
+// 
+const myFunc = callback
+
+function myFunc() {
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  Micro desafios:
+
+function andar(a) {
+    console.log('o carro está andando'+a)
+}
+function parar(a) {
+    console.log('o carro parou'+a)
+}
+function acaoCarro(callback) {
+    callback(a)
+}
+console.log(acaoCarro(parar))
