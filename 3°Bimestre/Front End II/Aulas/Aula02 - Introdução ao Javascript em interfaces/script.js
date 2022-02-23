69151c063f1c973f20fa1a6646f51ea476b374b3
andradeviniicius <viniciusdandrade01@gmail.com>
2022-02-22 21:37:33 -0300

    reolução com gracinha (que ficou bem mal feito...)

--- 3°Bimestre/Front End II/Aulas/Aula02 - Introdução ao Javascript em interfaces/script.js
+++ 3°Bimestre/Front End II/Aulas/Aula02 - Introdução ao Javascript em interfaces/script.js
@@ -1,58 +1,64 @@
-function getRandomNumber(min, max) {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min)) + min
-}
+function start() {
+    function getRandomNumber(min, max) {
+        min = Math.ceil(min)
+        max = Math.floor(max)
+        return Math.floor(Math.random() * (max - min)) + min
+    }
+    var Papel = document.getElementById('Papel')
+    var Tesoura = document.getElementById('Tesoura')
+    var Pedra = document.getElementById('Pedra')
 
-let userResult = prompt(`
-Essa questão é de vida ou morte, escolha com sabedoria
-1- Papel
-2- Tesoura
-3- Pedra
-`)
+    let userResult = 'Valor padrão'
+    let machineResult = getRandomNumber(1, 4)
 
-let machineResult = getRandomNumber(1, 4)
 
-switch (userResult) {
-    case '1':
-        userResult='Papel'
-        if(machineResult===1){
-            machineResult='Papel'
+    Papel.addEventListener("click", () => {
+        userResult = 1
+        if (machineResult === 1) {
+            userResult='Papel'
+            machineResult = 'Papel'
             alert(`O computador mostrou ${machineResult} e você mostrou ${userResult} portanto empatou`)
-        } else if (machineResult===2) {
-            machineResult='Tesoura'
+        } else if (machineResult === 2) {
+            userResult='Papel'
+            machineResult = 'Tesoura'
             alert(`O computador mostrou ${machineResult} e você mostrou ${userResult} portanto voce se f# demais`)
-        } else if (machineResult===3) {
-            machineResult='Pedra'
+        } else if (machineResult === 3) {
+            userResult='Papel'
+            machineResult = 'Pedra'
             alert(`O computador mostrou ${machineResult} e você mostrou ${userResult} portanto voce amassou o computador`)
         }
-        break;
-    case '2':
-        userResult='Tesoura'
-        if(machineResult===1){
-            machineResult='Papel'
+    })
+    Tesoura.addEventListener("click", () => {
+        userResult = 2
+        if (machineResult === 1) {
+            userResult='Tesoura'
+            machineResult = 'Papel'
             alert(`O computador mostrou ${machineResult} e você mostrou ${userResult} portanto voce amassou o computador`)
-        } else if (machineResult===2) {
-            machineResult='Tesoura'
+        } else if (machineResult === 2) {
+            userResult='Tesoura'
+            machineResult = 'Tesoura'
             alert(`O computador mostrou ${machineResult} e você mostrou ${userResult} portanto empatou`)
-        } else if (machineResult===3) {
-            machineResult='Pedra'
+        } else if (machineResult === 3) {
+            userResult='Tesoura'
+            machineResult = 'Pedra'
             alert(`O computador mostrou ${machineResult} e você mostrou ${userResult} portanto voce se f# demais`)
         }
-        break;
-    case '3':
-        userResult='Pedra'
-        if(machineResult===1){
-            machineResult='Papel'
+    })
+    Pedra.addEventListener("click", () => {
+        userResult = 3
+        if (machineResult === 1) {
+            userResult='Pedra'
+            machineResult = 'Papel'
             alert(`O computador mostrou ${machineResult} e você mostrou ${userResult} portanto voce se f# demais`)
-        } else if (machineResult===2) {
-            machineResult='Tesoura'
+        } else if (machineResult === 2) {
+            userResult='Pedra'
+            machineResult = 'Tesoura'
             alert(`O computador mostrou ${machineResult} e você mostrou ${userResult} portanto voce amassou o computador`)
-        } else if (machineResult===3) {
-            machineResult='Pedra'
+        } else if (machineResult === 3) {
+            userResult='Pedra'
+            machineResult = 'Pedra'
             alert(`O computador mostrou ${machineResult} e você mostrou ${userResult} portanto empatou`)
         }
-        break;
-    default:
-        alert('Voce nao escolheu nada portanto o computador acabou com sua raça')
+    })
+
 }
