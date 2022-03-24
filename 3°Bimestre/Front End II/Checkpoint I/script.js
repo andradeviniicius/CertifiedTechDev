f88b867c183cbbc75e3cf85afb3d9d63c278dc66
andradeviniicius <viniciusdandrade01@gmail.com>
2022-03-23 21:03:24 -0300

    Problema em upar imagens >(((

--- 3°Bimestre/Front End II/Checkpoint I/script.js
+++ 3°Bimestre/Front End II/Checkpoint I/script.js
@@ -5,41 +5,43 @@ const lastName = document.getElementById('lastName')
 const documentId = document.getElementById('documentId')
 const birthDate = document.getElementById('birthDate')
 
-let userFirstName = document.getElementById('userName')
-let userDocumentId = document.getElementById('userDocumentId')
-let userBirthDate = document.getElementById('userBirthDate')
-
-console.log(userBirthDate)
-
-const listaUsuario = [{
-  firstName:"",
-  lastName:"",
-  documentId:"",
-  birthDate:"",
-  userImage:"",
-}]
-
-
 
 submitBtn.addEventListener('click',(e)=>{
-  e.preventDefault();
-
-  userFirstName.value = (firstName.value + ' ' + lastName.value)
-  userDocumentId.value = documentId.value
-  userBirthDate.value = birthDate.value
-  let userImage = document.getElementById("userImage").files;
-
-    if (userImage.length > 0) {
-        var fileReader = new FileReader();
-
-        fileReader.onload = function (event) {
-            document.getElementById("userUserImage").setAttribute("src", event.target.result);
-        };
-
-        fileReader.readAsDataURL(userImage[0]);
-    }
+  // e.preventDefault();
 
 
+  window.sessionStorage.setItem('NomeSobrenome', (firstName.value + ' ' + lastName.value)) 
+  window.sessionStorage.setItem('CPF',  documentId.value)
+  window.sessionStorage.setItem('DataNascimento',  birthDate.value)
+  function getBase64Image(img) {
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+
+    var dataURL = canvas.toDataURL("image/png");
+
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+}
+
+  let userImage = document.getElementById("userImage");
+  let imgData = getBase64Image(userImage)
+  window.sessionStorage.setItem('imgData', imgData)
+
+  // window.sessionStorage.setItem('Img', JSON.stringify(userImage))
+      
+  //   if (userImage.length > 0) {
+  //         var fileReader = new FileReader();
+  
+  //         fileReader.onload = function (event) {
+  //             document.getElementById("userUserImage").setAttribute("src", event.target.result);
+  //           };
+            
+  //         fileReader.readAsDataURL(userImage[0]);
+  //     }
+      
 
 })
 
